ad01b4d086bac30974a9aad8b50e63c83f9b46a7
ridibrito <devoldbsb@gmail.com>
2022-10-15 07:22:16 -0300

    admin

---
+++ pages/admin.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function Admin() {
+    return (
+        <>
+        <div>
+            <Navbar />
+            <h1 className="text-2xl font-semibold text-center mt-3">Painel administrativo</h1>
+        </div>
+        </>
+    )
+}
