54a0c304089a5c6da93106ba41a3b84d2984a819
ricardo <linux@pop-os.localdomain>
2022-11-20 09:03:23 -0300

    sub

---
+++ prisma/migrations/20221120120110_initial2/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Abrangencia` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Acomodacao` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Coparticipacao` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Reembolso` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Abrangencia";
+
+-- DropTable
+DROP TABLE "Acomodacao";
+
+-- DropTable
+DROP TABLE "Coparticipacao";
+
+-- DropTable
+DROP TABLE "Reembolso";
